fb062b2dd03d52ff211b52329afa03b97eb17d66
OlehMelnyk1 <110389993+OlehMelnyk1@users.noreply.github.com>
2023-03-11 16:13:23 +0200

    add file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryBox = document.querySelector('.gallery');
+const galleryMurkup = createImageGalleryMurkup(galleryItems);
+
+function createImageGalleryMurkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+          <img 
+          class="gallery__image"   
+          src="${preview}"
+          alt="${description}"
+              </a>`
+    }).join('');
+}
+
+galleryBox.insertAdjacentHTML('beforeend', galleryMurkup);
+
+var galleryImg = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
